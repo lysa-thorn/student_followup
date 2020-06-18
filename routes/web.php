1748bd7c0bac8946f4318ae715ce742897ec9cb1
lysa-thorn <lysa.thorn@gmail.com>
2020-06-18 15:39:36 +0700

    out followup view

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('students', 'StudentController');
+Route::get('/returnOutFollowUpView', 'StudentController@returnOutFollowUpView')->name('returnOutFollowUpView');
 
 
 
